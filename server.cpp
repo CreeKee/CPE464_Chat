337d625da233a89478af8e97e2f9eb6fdbde1c4e
Seth Kiefer <sethk@stanford.edu>
2023-04-20 12:15:43 -0700

    used the stuff

--- server.cpp
+++ server.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	// wait for client to connect
 	clientSocket = tcpAccept(mainServerSocket, DEBUG_FLAG);
 
-	recvFromClient(clientSocket);
+	serverControl(mainServerSocket);
 	
 	/* close the sockets */
 	close(clientSocket);
