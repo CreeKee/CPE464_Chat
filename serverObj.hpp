7e9c7d7dcc77d5d552131af1ee3e797573574d5a
Seth Kiefer <sethk@stanford.edu>
2023-04-26 14:13:24 -0700

    added forwardM to flag actions

--- serverObj.hpp
+++ serverObj.hpp
@@ -23,11 +23,11 @@ class Server{
     private:
     void (Server::*flagActions[FLAGCOUNT])(FLAGACTION) = {
         &Server::cascadeB, &Server::handshake, &Server::errorFlag, 
-        &Server::errorFlag, &Server::errorFlag, &Server::errorFlag, 
+        &Server::errorFlag, &Server::errorFlag, &Server::forwardM, 
         &Server::errorFlag, &Server::errorFlag, &Server::errorFlag, 
         &Server::errorFlag, &Server::errorFlag, &Server::errorFlag, 
         &Server::errorFlag};
-        
+
     Clientele clientTable;
     int serverSocket;
 
