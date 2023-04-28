122e784e610a52e44d930f26c4215231382a7546
Seth Kiefer <sethk@stanford.edu>
2023-04-27 19:18:03 -0700

    sane

--- client.cpp
+++ client.cpp
@@ -222,9 +222,9 @@ void Client::displayCM(RECVACTION){
 
 	uint8_t offset = displayHandle(PDU)+1;
 
-	printf("num peers = %d\n",PDU[offset]);
+	printf("num peers = %d, %d\n",PDU[offset], PDU[offset+1]);
 	for(int dsts = 0; dsts < PDU[offset]; dsts++){
-		offset+= PDU[(1+offset)] + 2;
+		offset += PDU[(1+offset)] + 2;
 	}
 	printf("%s | %s | %d | %d\n",PDU, PDU+offset, PDU[offset], offset);
 }
